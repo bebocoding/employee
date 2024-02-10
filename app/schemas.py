1f28e642c74c9732d79759817aa59a0699b76eb8
Abdallah Mohy <bebostudy@gmail.com>
2024-02-10 20:32:58 +0200

    initial commit

---
+++ app/schemas.py
@@ -0,0 +1,68 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+# Request Schema
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+# Response Schema
+
+
+class Post(PostBase):
+    owner_id: int
+    id: int
+    created_at: datetime
+    owner: UserOut
+
+    class Config:
+        orm_mode = True  # for pydantic to accept orm types
+
+
+class PostOut(BaseModel):
+    Post: Post
+    votes: int
+
+    class Config:
+        orm_mode = True  # for pydantic to accept orm types
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: Optional[int] = None
+
+
+class Vote(BaseModel):
+    post_id: int
+    dir: bool
