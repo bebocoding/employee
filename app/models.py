804513db9fe9f77f29b445859c21b5e7358d22c1
Abdallah Mohy <bebostudy@gmail.com>
2024-02-25 23:54:35 +0200

    Added Docker

--- app/models.py
+++ app/models.py
@@ -26,7 +26,7 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False,
                         server_default=text('now()'))
-    phone = Column(String)
+    phone_number = Column(String)
 
     posts = relationship("Post", back_populates="owner")
 
